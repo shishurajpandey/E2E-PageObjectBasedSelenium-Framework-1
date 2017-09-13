e60844ca187447bedf7a9af777677db99f3b7569
SHISHU RAJ PANDEY <SHISHU RAJ PANDEY@ShishuRajPandey>
2017-09-13 14:46:20 +0530

    Commit-9/13/2017

--- PageObjectE2EFramework/src/main/java/com/automation/uiautomation/excelReader/Excel_Reader.java
+++ PageObjectE2EFramework/src/main/java/com/automation/uiautomation/excelReader/Excel_Reader.java
@@ -1,9 +1,11 @@
 package com.automation.uiautomation.excelReader;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Excel_Reader {
@@ -25,9 +27,50 @@ public class Excel_Reader {
 	}
 	
 	
-	public void getDataFromSheet()
+	@SuppressWarnings("deprecation")
+	public String[][] getDataFromSheet(String sheetName)
 	{
+		String[][] dataSets=null;
+		try{
+			XSSFSheet sheet=workbook.getSheet(sheetName);
 		
+		int totalRows=sheet.getLastRowNum()+1;
+		int totalColoums=sheet.getRow(0).getLastCellNum();
+		dataSets=new String[totalRows-1][totalColoums];
+		
+		for(int i=1;i<totalRows-1;i++)
+		{ XSSFRow row=sheet.getRow(i);
+		for(int j=0;j<totalColoums-1;j++)
+		{
+			XSSFCell cell=row.getCell(j);
+			
+			if(cell.getCellType()==org.apache.poi.ss.usermodel.Cell.CELL_TYPE_STRING)
+			{
+				dataSets[i-1][j]=cell.getStringCellValue();
+				
+			}
+			else
+				if(cell.getCellType()==Cell.CELL_TYPE_NUMERIC)
+				{
+					dataSets[i-1][j]=String.valueOf(cell.getNumericCellValue());
+					
+				}
+				else
+					if(cell.getCellType()==Cell.CELL_TYPE_BOOLEAN)
+					{
+						dataSets[i-1][j]=String.valueOf(cell.getBooleanCellValue());
+						
+					}
+		}
+			return dataSets;
+		}
+		
+		}
+		catch(Exception E)
+		{
+			E.printStackTrace();
+		}
+		return dataSets;
 	}
 	
 	
